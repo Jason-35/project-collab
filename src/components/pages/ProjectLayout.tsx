2063318d78a5debbb36849512b3ef2e6a18248da
Jason <jsonwu34@gmail.com>
2023-12-14 23:15:34 -0500

    reloads on firestore data updates

--- src/components/pages/ProjectLayout.tsx
+++ src/components/pages/ProjectLayout.tsx
@@ -1,13 +1,22 @@
 import { useParams } from "react-router-dom";
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+import { db } from "../../firebase/firebase";
+import { query, collection, limit } from "firebase/firestore";
 
 const ProjectLayout = () => {
 
     const { uuid, name } = useParams()
 
-    console.log(name, uuid)
+    // const messageRef = db.collection("messages")
+    const messageRef = query(collection(db, "messages"), limit(10))
 
+    const [messages] = useCollectionData(messageRef)
+
+    console.log(name, uuid)
+    console.log(messages)
     return ( 
-        <div>start of a new project</div>
+        <div>start of a new project {messages && (messages.length > 0) && messages[0].message}</div>
      );
 }
  
